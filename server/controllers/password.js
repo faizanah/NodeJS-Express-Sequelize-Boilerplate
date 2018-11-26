9c735aa3c6644ae27c86e78d0ac40ba80795bbeb
Faizan AH <faizuali4@hmail.com>
2018-11-26 13:58:38 +0100

    added user password change endpoint

--- server/controllers/password.js
+++ server/controllers/password.js
@@ -1,9 +1,6 @@
-
 const bcrypt     = require('bcrypt');
 const salt       = bcrypt.genSaltSync(10);
-const db         = require('../config/db');
 function changePassword(req , res) {
-
     var data = {
         currentPassword: req.body.current_password,
         newPassword: req.body.new_password,
@@ -13,18 +10,17 @@ function changePassword(req , res) {
     {
         if(req.user.authenticate(data.currentPassword)){
             req.user.updateAttributes({password: bcrypt.hashSync(data.newPassword, salt, null)}).then(function(result){
-                return res.status(200).send({success: true ,message: "Password successfully changed."});
+              result.sendChangePasswordNotification();
+              return res.status(200).send({success: true ,message: "Password successfully changed."});
             }).catch(function(error){
-                console.log(JSON.stringify(error , null ,2));
-                db.sendError(error,res)
+                req.db.sendError(error,res)
             });
         }else{
-            return db.sendError({code: 422 , message: 'Current password does not match.'},res);
+            return req.db.sendError({code: 422 , message: 'Current password does not match.'},res);
         }
     }else{
-        db.sendError({message: 'Confirm password does not match'} , res)
+        req.db.sendError({message: 'Confirm password does not match'} , res)
     }
-
 }
 
 module.exports = {
